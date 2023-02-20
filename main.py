f6322774e8ee503147819197cbf293372ca07469
Szybki0o0 <105429743+Szybki0o0@users.noreply.github.com>
2023-02-20 21:39:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,172 @@
 import pygame
 
-# Inicjalizacja modulu pygame i wyświetlacza
+# Initialization of pygame module and display
 pygame.init()
 pygame.display.init()
 
-# Ustawienie parametrów dla okna jak rozmiar i nazwa
+# Function called when someone wins
+def endWin(result):
+    pygame.draw.rect(screen,black,pygame.Rect(60,60,550,550))
+    screen.blit(title.render(f'Winner: {result}', False, white), (150, 60))
+    screen.blit(table.render(f'Number of wins', False, white), (180, 240))
+    screen.blit(table.render(f'X: {nowX}       O: {nowO}', False, white), (210, 300))
+    screen.blit(press.render(f'Press Space to continue', False, white), (210, 480))
+    global isEnd
+    isEnd = True
+
+# Function called when there is a draw
+def endDraw(x,o):
+    pygame.draw.rect(screen,black,pygame.Rect(60,60,550,550))
+    screen.blit(title.render(f'Draw', False, white), (220, 60))
+    screen.blit(table.render(f'Number of wins', False, white), (180, 240))
+    screen.blit(table.render(f'X: {x}       O: {o}', False, white), (210, 300))
+    screen.blit(press.render(f'Press Space to continue', False, white), (210, 480))
+    global isEnd
+    isEnd = True
+
+# Setting parameters for screen
 def SettingWindow():
+    # Screen, its size and display name
     (width, height) = 650, 650
     global screen
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("TicTacToe")
+    # 3 fonts of different size
+    global title
+    global table
+    global press
+    pygame.font.init()
+    title = pygame.font.SysFont('Copper Black', 100)
+    table = pygame.font.SysFont('Copper Black', 50)
+    press = pygame.font.SysFont('Copper Black', 25)
 
-# Główna funkcja odpowiedzialna za to co dzieje się na ekranie
+# Main function responsible for what's on the screen
 def StartingWindow():
-    # Deklaracja zmiennych pomocniczych
+    # Declaration of assistant variables
+    global isEnd
+    global board
+    global nowX
+    global nowO
+    nowX = 0
+    nowO = 0
+    board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]] # list representing game board
     circleTurn = False
     isRunning = True
+    isEnd = False
     activeCoordinateLeft = 0
     activeCoordinateTop = 0
     pygame.display.flip()
-    # Główna pętla programu sprawdzająca na bieżąco aktywność użytkownika
+    # Main program loop monitoring user actions
     while isRunning:
-        # Pętla odpowiedzialna za aktualizowanie wyświetlania figur
+        # Loop responsible for dynamic actualizing box content
         for element in rectList:
-            # Usuwanie elemntów z listy gdy została już do nich przypisana figura oraz zmiana tury
+            # Deleting elements from list when there is figure assigned to it and turn change
             if element.left == activeCoordinateLeft and element.top == activeCoordinateTop:
-                # Zmiana tury
+                # Turn change
                 if circleTurn:
                     circleTurn = False
                 else:
                     circleTurn = True
                 rectList.remove(pygame.Rect(activeCoordinateLeft,activeCoordinateTop,160,160))
+                if len(rectList) == 0 and isEnd == False:
+                    endDraw(nowX,nowO)
                 pygame.display.update()
             else:
-                # Aktualizowanie pozycji figury do potencjalnego kwadratu
-                if element.collidepoint(pygame.mouse.get_pos()):
-                    # Dla kółka
+                # Actualising figure position to potential box
+                if element.collidepoint(pygame.mouse.get_pos()) and isEnd == False:
+                    # For o
                     if circleTurn:
                         pygame.draw.circle(screen, grey,[element.left+80,element.top+80] , 60,15)
                         pygame.display.update()
-                    # Dla krzyżyka
+                    # For x
                     else:
                         pygame.draw.line(screen,white,[element.left+30,element.top+30],[element.left+130,element.top+130],25)
                         pygame.draw.line(screen,white,[element.left+30,element.top+130],[element.left+130,element.top+30],25)
                         pygame.display.update()
-                # Zamalowywanie niepotrzenej figury aby nie pozostawała na niechcianej pozycji
-                else:
+                # Covering unwanted figure so that it doesn't stay in a box
+                elif isEnd == False:
                     pygame.draw.rect(screen, black, pygame.Rect(element.left+5, element.top+5, 150, 150))
         # Pętla odpowiedzialna za akcje użytkownika
+        # Loop responsible for user actions
         for event in pygame.event.get():
-            # Warunek monitorujący przyciśnięcie przycisku myszki
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and isEnd:
+                    pygame.draw.rect(screen, black, pygame.Rect(60, 60, 550, 550))
+                    isEnd = False
+                    board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+                    activeCoordinateLeft = 0
+                    activeCoordinateTop = 0
+                    DrawingBoard()
+            # Condition monitoring if mouse button was clicked
             if event.type == pygame.MOUSEBUTTONDOWN:
-                # Pętla aktualizująca aktywne pozycje chcianego kwadratu
+                # Loop actualising active box position
                 for element in rectList:
                     if element.collidepoint(pygame.mouse.get_pos()):
                         activeCoordinateLeft = element.left
                         activeCoordinateTop = element.top
                         pygame.display.update()
-            # Warunek monitorujący czy użytkownik zamknął okno
+
+                        # Checking in which column is new figure
+                        if activeCoordinateLeft == 60:
+                            i = 0
+                        elif activeCoordinateLeft == 240:
+                            i = 1
+                        elif activeCoordinateLeft == 420:
+                            i = 2
+
+                        # Checking in which row is new figure
+                        if activeCoordinateTop == 60:
+                            j = 0
+                        elif activeCoordinateTop == 240:
+                            j = 1
+                        elif activeCoordinateTop == 420:
+                            j = 2
+
+                        # Editing list
+                        if circleTurn:
+                            board[j][i] = 'O'
+                        else:
+                            board[j][i] = 'X'
+
+                        if isEnd == False:
+                            # Checking columns
+                            for i in range(3):
+                                if board[0][i] == board[1][i] and board[0][i] == board[2][i]:
+                                    if board[0][i] == 'X':
+                                        nowX += 1
+                                    else:
+                                        nowO += 1
+                                    endWin(board[0][i])
+                                    break
+                            # Checking rows
+                            for i in range(3):
+                                if board[i][0] == board[i][1] and board[i][0] == board[i][2]:
+                                    if board[i][0] == 'X':
+                                        nowX += 1
+                                    else:
+                                        nowO += 1
+                                    endWin(board[i][0])
+                                    break
+                            # Checking diagonals
+                            if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+                                if board[1][1] == 'X':
+                                    nowX += 1
+                                else:
+                                    nowO += 1
+                                endWin(board[1][1])
+                            elif board[2][0] == board[1][1] and board[2][0] == board[0][2]:
+                                if board[1][1] == 'X':
+                                    nowX += 1
+                                else:
+                                    nowO += 1
+                                endWin(board[1][1])
+            # Condition monitoring if window is closed
             if event.type == pygame.QUIT:
                 isRunning = False
             if isRunning == False:
                 pygame.quit()
 
-# Funkcja odpowiedzialna za ustawienie i przechowywanie kolorów potrzebnych na planszy
+# Function responsible for setting and storing colors needed on the board
 def SettingColors():
     global white
     global black
@@ -80,23 +183,23 @@ def SettingColors():
     lightRed = (219,80,74)
     lightYellow = (255,209,102)
 
-# Wywołanie dwóch wyżej opisanych funkcji
+# Function call
 SettingColors()
 SettingWindow()
 
-# Funkcja rysująca planszę gry i definiująca poszczególne pola gry
+# Function drawind game board and defining boxes
 def DrawingBoard():
-    # Narysowanie lini
+    # Drawing lines
     pygame.draw.line(screen, lightBlue,[230,60],[230,580],20)
     pygame.draw.line(screen, lightYellow,[60,410],[580,410],20)
     pygame.draw.line(screen, lightGreen,[60,230],[580,230],20)
     pygame.draw.line(screen, lightRed,[410,60],[410,580],20)
 
-    # Korekcja dwóch lini tak, aby się przeplatały
+    # Corecting two lines so they interwine
     pygame.draw.rect(screen,lightBlue,pygame.Rect(221,221,20,20))
     pygame.draw.rect(screen,lightGreen,pygame.Rect(401,221,20,20))
 
-    # Stworzenie i dodanie do listy poszczególnych 9 pól gry
+    # Creation and addition 9 boxes to list
     global rectList
     rectList = []
     x = 60
@@ -116,7 +219,6 @@ def DrawingBoard():
                 x = 60
                 y+=180
 
-# Wywołanie wyżej opisanych funkcji
+# Function call
 DrawingBoard()
 StartingWindow()
-
